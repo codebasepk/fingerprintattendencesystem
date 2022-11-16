6ab1c1ca2776022ad35ed08b507dce30bf03c6d0
SHAKEEB KHAN <shakeebsun@gmail.com>
2022-11-16 23:51:07 +0500

    Get Register User Data To Show

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('getdata/', views.ProfileView.as_view(), name='getdata'),
     path('getdata/<int:pk>/', views.ProfileView.as_view(), name='getdata'),
     path('register/', views.RegisterView.as_view(), name='register'),
+    path('registergetdata/', views.RegisterView.as_view(), name='registergetdata'),
+    path('registergetdata/<int:pk>/', views.RegisterView.as_view(), name='registergetdata'),
     path('update/<int:pk>/', views.ProfileView.as_view(), name='update'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from .models import FingerprintProfileModel
+from .models import FingerprintProfileModel, RegisterPersonModel
 from .serializers import FingerprintProfileSerializer
 from .serializers import RegisterPersonSerializer
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-import io
+from rest_framework.filters import SearchFilter
 
 
 class RegisterView(APIView):
@@ -21,6 +17,16 @@ class RegisterView(APIView):
                 'status': 'success', 'candidate': serializer.data},
                 status=status.HTTP_201_CREATED)
         return Response(serializer.errors)
+
+    def get(self, request, pk=None, format=None):
+        id = pk
+        if id is not None:
+            candidates = RegisterPersonModel.objects.get(id=id)
+            serializer = RegisterPersonSerializer(candidates)
+            return Response({'status': 'success', 'candidates': serializer.data}, status=status.HTTP_200_OK)
+        candidates = RegisterPersonModel.objects.all()
+        serializer = RegisterPersonSerializer(candidates, many=True)
+        return Response({'status': 'success', 'candidates': serializer.data}, status=status.HTTP_200_OK)
 
 
 class ProfileView(APIView):
